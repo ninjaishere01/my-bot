23e225d21d5daf72fbe09f4935375f4b5db1cb03
ninjaishere01 <ninjaishere01@gmail.com>
2026-09-24 03:28:32 +0300

    Update app.py
    done

--- app.py
+++ app.py
@@ -36,9 +36,13 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("telegram").setLevel(logging.WARNING)
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-raw_channel_id = os.environ.get("CHANNEL_ID")
-CHANNEL_ID = int(raw_channel_id) if raw_channel_id else 0
-CHANNEL_INVITE_LINK = os.environ.get("CHANNEL_INVITE_LINK")
+raw_channel_id = os.environ.get("CHANNEL_ID", "0")
+try:
+    CHANNEL_ID = int(str(raw_channel_id).strip())
+except ValueError:
+    CHANNEL_ID = 0
+
+CHANNEL_INVITE_LINK = os.environ.get("CHANNEL_INVITE_LINK", "")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
